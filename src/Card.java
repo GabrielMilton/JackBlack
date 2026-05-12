1ac1e4e314b79838b10e29f4e8d7ca4c9ef9c747
GabrielMilton <gabriel_galva-diez28@milton.edu>
2026-05-12 10:21:39 -0400

    Worked on it

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,40 @@
+import javax.xml.soap.Name;
+
 public class Card {
     public String Suit;
     public int Value;
     public String Name;
+    //todo: make a consturuncor that has three parameters one for each vatriable. Set the tinstance varibles accroding to the prapmeters
+    //todo: make a print info that prints the info of the card
+    public Card(String psuit,int pvalue,int pName){
+        Value = pvalue;
+        Suit = psuit;
+        Name = "Ace";
+        if (pName == 0){
+            Name = "Ace";
+            Value = 11;
+        }
+        else if(pName == 1){
+            Name = "two";
+            Value = 2;
+        }
+        else if(pName == 2){
+            Name = "three";
+            Value = 3;
+        }
+        else if(pName == 3){
+            Name = "four";
+            Value = 4;
+        }
+        else if(pName == 4){
+            Name = "five";
+            Value = 5;
+        }
+    }
+
+
+    public void Printinfo(){
+        System.out.println("the " + Name + " of " + Suit + " is worth " + Value + " points" );
+
+    }
 }
--- src/MainGame.java
+++ src/MainGame.java
@@ -3,8 +3,17 @@ public class MainGame {
     public String Player;
     public String Dealer;
     public static void main(String[] args) {
-
+        new MainGame();
     }
+    public MainGame(){
+        System.out.println("Welcome to Blackjack");
+        Deck = new Card[52];
+        for(int x=0; x<13; x++){
+            Deck[x] = new Card("Dimonds",10,x);
+            Deck[x].Printinfo();
+        }
+    }
+
     public void compare(){
     }
 
