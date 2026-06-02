b9af04fd87b9f9a836fb7b25de7e9944f30f111e
GabrielMilton <gabriel_galva-diez28@milton.edu>
2026-06-02 14:01:07 -0400

    The whole game. :)

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.xml.ws.Endpoint;
+import java.awt.*;
 import java.util.Scanner;
 
 public class MainGame {
@@ -17,7 +19,7 @@ public class MainGame {
         int Counter = 0;
         for (int y = 0; y < 4; y = y + 1) {//4 suits per deck
             for (int x = 0; x < 13; x++) {//13 cards per suit
-                Deck[Counter] = new Card(y, 10, x);
+                Deck[Counter] = new Card(y,x, x);
                 Counter++;
 
             }
@@ -53,7 +55,7 @@ public class MainGame {
         Counting = 4;
 
 //todo: Add a counter for the dealer to check how many cars ot gabe out and increasde by one
-        while (me.isHit == true) {
+        while (me.isHit == true && me.isBust == false) {
             ExtraCards = new Card[me.Hand.length + 1];
             for (int t = 0; t < me.Hand.length; t = t + 1) {
                 ExtraCards[t] = me.Hand[t];
@@ -65,31 +67,53 @@ public class MainGame {
             me.calulateTotal();
             System.out.println(me.Cardtotal);
             me.HitorStay();
+            if(me.Cardtotal > 21) {
+                System.out.println("You busted! You lost!:(");
+                me.isBust = true;
+            }
+            if (me.Cardtotal == 21) {
+                System.out.println("You got BlackJack! You Won" + "\n" + "You beat Niam and now get 1000000000$!:)");
+                me.isBust = true;
+            }
         }
+
 
 
 
 
         if (me.isHit == false) {
-            {
+
                 System.out.println("The dealer's amount is " + Niam.Cardtotal);
                 if (Niam.Cardtotal < 16) {
-                    ExtraCards = new Card[3];
+                    ExtraCards = new Card[Niam.Hand.length +1];
                     for (int t = 0; t < Niam.Hand.length; t = t + 1) {
                         ExtraCards[t] = Niam.Hand[t];
                     }
-                    ExtraCards[2] = Deck[4];
+                    Counting = Counting + 1;
+                    ExtraCards[2] = Deck[Counting];
                     Niam.Hand = ExtraCards;
+                    Niam.calulateTotal();
                     System.out.println("The dealer has under 16 and decided to hit and now they have " + Niam.Cardtotal);
                 }
                 if (Niam.Cardtotal > 16) {
-                    System.out.println("The dealer has over 16 and can't hit ");
-                    compare();
-                }
+                System.out.println("The dealer has over 16 and can't hit ");
+                compare();
             }
 
+                else if (Niam.Cardtotal > 21) {
+                    System.out.println("The dealer has over 21! They busted! ");
+                    System.out.println("You Won!");
+                }
+                if(Niam.Cardtotal > 21 && me.Cardtotal >21 ){
+                    System.out.println("Both you and the dealer have over 21, so you both lose");
+                }
+
+
+
         }
-    }
+
+        }
+
 
     public void compare() {
         System.out.println("The Dealer has a value of: " + Niam.Cardtotal);
@@ -97,13 +121,22 @@ public class MainGame {
             me.isBust = true;
             System.out.println("You Busted! " + "\n" +  "You lost!");
         }
-        if (me.Cardtotal < Niam.Cardtotal){
+        if (me.Cardtotal < Niam.Cardtotal && Niam.Cardtotal <= 21){
             System.out.println("The dealer has a higher number than you! " + "\n" +  "You lost!");
         }
 
-        if (me.Cardtotal <= 21) {
+
+        if (me.Cardtotal > Niam.Cardtotal && Niam.Cardtotal <= 21){
+            System.out.println("The dealer has a higher number than you! " + "\n" +  "You lost!");
+        }
+
+        if (me.Cardtotal <= 21 && Niam.Cardtotal<me.Cardtotal) {
             me.isBust = false;
             System.out.println("You are under 21! " + "\n" +  "You Won!");
+        }
+        if (me.Cardtotal <= 21 && Niam.Cardtotal>me.Cardtotal &&  Niam.Cardtotal <= 21) {
+            me.isBust = false;
+            System.out.println("You are under 21 but the dealer has a high number. The dealer won! " + "\n" +  "You lost!:(");
         }
     }
 
